90e53fc407e5e9c0d9e8578c941e1dc972de9e05
separatrixxx <nikitalochmatov@gmail.com>
2025-02-01 13:09:38 +0300

    fix zoom

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700;900&display=swap" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </Head>
       <body>
         <Main />
